945f00b747445953eb1d238274d6cb92e41375d6
Brandon Creed <brandon.f.creed@gmail.com>
2023-08-10 14:50:44 -0400

    Updated single yacht page significantly. Looking alot more like the design

--- partials/single-yacht.php
+++ partials/single-yacht.php
@@ -85,6 +85,144 @@ get_header();
                         </div>
                     </div>
                 </div>
+                <div class="yacht-download-brochure-container">
+                    <button class="yacht-download-button">
+                        <img src="<?php echo RAI_YS_PLUGIN_ASSETS; ?>images/download.png" alt="download-icon" />
+                        Download Brochure
+                    </button>
+                </div>
+                <div class="yacht-general-description-container">
+                    <p class="yacht-description-label">
+                        Description
+                    </p>
+                    <div class="yacht-description-value">
+                        <?php echo ($vessel->GeneralBoatDescription[0]) ?>
+                    </div>
+                </div>
+                <div class="yacht-specs-container">
+                    <p class="yacht-specs-label">
+                        Specifications
+                    </p>
+                    <div class="yacht-specs-value">
+                        <div class="yacht-specs-item">
+                            <p class="specification-name">Make</p>
+                            <p class="specification-value"><?php echo empty($vessel->MakeString) ? "N/A" : $vessel->MakeString; ?></p>
+                        </div>
+                        <div class="yacht-specs-item">
+                            <p class="specification-name">Model</p>
+                            <p class="specification-value"><?php echo empty($vessel->Model) ? "N/A" : $vessel->Model; ?></p>
+                        </div>
+                        <div class="yacht-specs-item">
+                            <p class="specification-name">Year</p>
+                            <p class="specification-value"><?php echo empty($vessel->ModelYear) ? "N/A" : $vessel->ModelYear; ?></p>
+                        </div>
+                        <div class="yacht-specs-item">
+                            <p class="specification-name">Class</p>
+                            <p class="specification-value"><?php echo empty($vessel->BoatCategoryCode) ? "N/A" : $vessel->BoatCategoryCode; ?></p>
+                        </div>
+                        <div class="yacht-specs-item">
+                            <p class="specification-name">Price</p>
+                            <p class="specification-value"><?php echo empty($vessel->Price) ? "N/A" : '$' . $vessel->Price; ?></p>
+                        </div>
+                        <div class="yacht-specs-item">
+                            <p class="specification-name">Fuel Tank Capacity</p>
+                            <p class="specification-value"><?php echo empty($vessel->FuelTankCapacityMeasure) ? "N/A" : $vessel->FuelTankCapacityMeasure; ?></p>
+                        </div>
+                        <div class="yacht-specs-item">
+                            <p class="specification-name">Hull Material</p>
+                            <p class="specification-value"><?php echo empty($vessel->BoatHullMaterialCode) ? "N/A" : $vessel->BoatHullMaterialCode; ?></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="yacht-full-details-container">
+                    <p class="yacht-details-label">
+                        Full Details
+                    </p>
+                    <div class="yacht-details-value">
+                        <div class="yacht-accordion-item">
+                            <p class="yacht-accordion-name">
+                                General Info
+                            </p>
+                            <p class="yacht-accordion-arrow">
+                                <img src="<?php echo RAI_YS_PLUGIN_ASSETS; ?>images/chevron-down.png" alt="down-arrow-icon" />
+                            </p>
+                        </div>
+                        <div class="yacht-accordion-display">
+                            <p class="yacht-accordion-display-item">Make: <?php echo empty($vessel->MakeString) ? "N/A" : $vessel->MakeString; ?></p>
+                            <p class="yacht-accordion-display-item">Model: <?php echo empty($vessel->Model) ? "N/A" : $vessel->Model; ?></p>
+                            <p class="yacht-accordion-display-item">Condition: <?php echo empty($vessel->SaleClassCode) ? "N/A" : $vessel->SaleClassCode; ?></p>
+                            <p class="yacht-accordion-display-item">Class: <?php echo empty($vessel->BoatCategoryCode) ? "N/A" : $vessel->BoatCategoryCode; ?></p>
+                            <p class="yacht-accordion-display-item">Construction: <?php echo empty($vessel->BoatHullMaterialCode) ? "N/A" : $vessel->BoatHullMaterialCode; ?></p>
+                            <p class="yacht-accordion-display-item">Boat Hull ID: <?php echo empty($vessel->BoatHullID) ? "N/A" : $vessel->BoatHullID; ?></p>
+                            <p class="yacht-accordion-display-item">Has Hull ID: <?php echo empty($vessel->HasBoatHullID) ? "No" : ($vessel->HasBoatHullID == '1' ? 'Yes' : "N/A");  ?></p>
+                        </div>
+                        <div class="yacht-accordion-item">
+                            <p class="yacht-accordion-name">
+                                Measurements
+                            </p>
+                            <p class="yacht-accordion-arrow">
+                                <img src="<?php echo RAI_YS_PLUGIN_ASSETS; ?>images/chevron-down.png" alt="down-arrow-icon" />
+                            </p>
+                        </div>
+                        <div class="yacht-accordion-display">
+                            <p class="yacht-accordion-display-item">Length: <?php echo empty($vessel->NominalLength) ? "N/A" : $vessel->NominalLength . " / " . round((float)$vessel->NominalLength * 0.3048, 2) . ' m'; ?></p>
+                            <p class="yacht-accordion-display-item">Length Overall: <?php echo empty($vessel->LengthOverall) ? "N/A" : $vessel->LengthOverall . " / " . round((float)$vessel->LengthOverall * 0.3048, 2) . ' m'; ?></p>
+                            <p class="yacht-accordion-display-item">Beam: <?php echo empty($vessel->BeamMeasure) ? "N/A" : $vessel->BeamMeasure . " / " . round((float)$vessel->BeamMeasure * 0.3048, 2) . ' m'; ?></p>
+                        </div>
+                        <?php if (!empty($vessel->Engines[0])){ ?>
+                            <div class="yacht-accordion-item">
+                                <p class="yacht-accordion-name">
+                                    Engine 1
+                                </p>
+                                <p class="yacht-accordion-arrow">
+                                    <img src="<?php echo RAI_YS_PLUGIN_ASSETS; ?>images/chevron-down.png" alt="down-arrow-icon" />
+                                </p>
+                            </div>
+                            <div class="yacht-accordion-display">
+                                <p class="yacht-accordion-display-item">Make: <?php echo empty($vessel->Engines[0]->Make) ? "N/A" : $vessel->Engines[0]->Make; ?></p>
+                                <p class="yacht-accordion-display-item">Model: <?php echo empty($vessel->Engines[0]->Model) ? "N/A" : $vessel->Engines[0]->Model; ?></p>
+                                <p class="yacht-accordion-display-item">Fuel: <?php echo empty($vessel->Engines[0]->Fuel) ? "N/A" : $vessel->Engines[0]->Fuel; ?></p>
+                                <p class="yacht-accordion-display-item">Engine Power: <?php echo empty($vessel->Engines[0]->EnginePower) ? "N/A" : $vessel->Engines[0]->EnginePower; ?></p>
+                                <p class="yacht-accordion-display-item">Type: <?php echo empty($vessel->Engines[0]->Type) ? "N/A" : $vessel->Engines[0]->Type; ?></p>
+                                <p class="yacht-accordion-display-item">Engine Hours: <?php echo empty($vessel->Engines[0]->Hours) ? "N/A" : $vessel->Engines[0]->Hours; ?></p>
+                            </div>
+                        <?php } if (!empty($vessel->Engines[1])){ ?>
+                            <div class="yacht-accordion-item">
+                                <p class="yacht-accordion-name">
+                                    Engine 2
+                                </p>
+                                <p class="yacht-accordion-arrow">
+                                    <img src="<?php echo RAI_YS_PLUGIN_ASSETS; ?>images/chevron-down.png" alt="down-arrow-icon" />
+                                </p>
+                            </div>
+                            <div class="yacht-accordion-display">
+                                <p class="yacht-accordion-display-item">Make: <?php echo empty($vessel->Engines[1]->Make) ? "N/A" : $vessel->Engines[1]->Make; ?></p>
+                                <p class="yacht-accordion-display-item">Model: <?php echo empty($vessel->Engines[1]->Model) ? "N/A" : $vessel->Engines[1]->Model; ?></p>
+                                <p class="yacht-accordion-display-item">Fuel: <?php echo empty($vessel->Engines[1]->Fuel) ? "N/A" : $vessel->Engines[1]->Fuel; ?></p>
+                                <p class="yacht-accordion-display-item">Engine Power: <?php echo empty($vessel->Engines[1]->EnginePower) ? "N/A" : $vessel->Engines[1]->EnginePower; ?></p>
+                                <p class="yacht-accordion-display-item">Type: <?php echo empty($vessel->Engines[1]->Type) ? "N/A" : $vessel->Engines[1]->Type; ?></p>
+                                <p class="yacht-accordion-display-item">Engine Hours: <?php echo empty($vessel->Engines[1]->Hours) ? "N/A" : $vessel->Engines[1]->Hours; ?></p>
+                            </div>
+                        <?php } if (!empty($vessel->Engines[2])) { ?>
+                            <div class="yacht-accordion-item">
+                                <p class="yacht-accordion-name">
+                                    Engine 3
+                                </p>
+                                <p class="yacht-accordion-arrow">
+                                    <img src="<?php echo RAI_YS_PLUGIN_ASSETS; ?>images/chevron-down.png" alt="down-arrow-icon" />
+                                </p>
+                            </div>
+                            <div class="yacht-accordion-display">
+                                <p class="yacht-accordion-display-item">Make: <?php echo empty($vessel->Engines[2]->Make) ? "N/A" : $vessel->Engines[2]->Make; ?></p>
+                                <p class="yacht-accordion-display-item">Model: <?php echo empty($vessel->Engines[2]->Model) ? "N/A" : $vessel->Engines[2]->Model; ?></p>
+                                <p class="yacht-accordion-display-item">Fuel: <?php echo empty($vessel->Engines[2]->Fuel) ? "N/A" : $vessel->Engines[2]->Fuel; ?></p>
+                                <p class="yacht-accordion-display-item">Engine Power: <?php echo empty($vessel->Engines[2]->EnginePower) ? "N/A" : $vessel->Engines[2]->EnginePower; ?></p>
+                                <p class="yacht-accordion-display-item">Type: <?php echo empty($vessel->Engines[2]->Type) ? "N/A" : $vessel->Engines[2]->Type; ?></p>
+                                <p class="yacht-accordion-display-item">Engine Hours: <?php echo empty($vessel->Engines[2]->Hours) ? "N/A" : $vessel->Engines[2]->Hours; ?></p>
+                            </div>
+                        <?php } ?>
+                    </div>
+                </div>
             </div>
             <div class="secondary-container">
                 
