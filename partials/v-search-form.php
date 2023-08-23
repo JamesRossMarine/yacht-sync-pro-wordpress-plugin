c7472e512f80ac8e13f98a91e95220ec9e1b803e
Brandon Fernandez Creed <brandon.f.creed@gmail.com>
2023-08-23 13:40:09 -0400

    worked on the vertical search form for the plugin

---
+++ partials/v-search-form.php
@@ -0,0 +1,83 @@
+<div id="v-search-container">
+    <form id="v-search-form" class="ys-v-row">
+        <div class="ys-v-row-item">
+            <label>Keyword</label>
+            <input type="text" name="keyword" placeholder="Search by Name" />
+        </div>
+        <div class="ys-v-row-item">
+            <label>Builder</label>
+            <select name="builder">
+                <option value="" selected disabled>Any</option>
+                <option value="Admiral">Admiral</option>
+                <option value="Azimut">Azimut</option>
+                <option value="Baglietto">Baglietto</option>
+                <option value="Benetti">Benetti</option>
+                <option value="Ferretti">Ferretti</option>
+                <option value="Mangusta">Mangusta</option>
+            </select>
+        </div>
+        <div class="ys-v-row-item">
+            <label>Year</label>
+            <div class="min-max-container">
+                <input type="number" name="year-min" placeholder="Min"/>
+                <span>-</span>
+                <input type="number" name="year-max" placeholder="Max"/>
+            </div>
+        </div>
+        <div class="ys-v-row-item">
+            <label>Length</label>
+            <div class="min-max-container">
+                <input type="number" name="length-min" placeholder="Min"/>
+                <span>-</span>
+                <input type="number" name="length-max" placeholder="Max"/>
+            </div>
+        </div>
+        <div class="ys-v-row-item">
+            <label>Price</label>
+            <div class="min-max-container">
+                <input type="number" name="price-min" placeholder="Min"/>
+                <span>-</span>
+                <input type="number" name="price-max" placeholder="Max"/>
+            </div>
+        </div>
+        <div class="ys-v-row-item">
+            <label>Staterooms</label>
+            <select name="staterooms">
+                <option value="" selected disabled>Any</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+            </select>
+        </div>
+        <div class="ys-v-row-item">
+            <label>Hull</label>
+            <select name="hull">
+                <option value="" selected disabled>Any</option>
+                <option value="Aluminum">Aluminum</option>
+                <option value="Composite">Composite</option>
+                <option value="Fiberglass">Fiberglass</option>
+                <option value="Resin">Resin</option>
+                <option value="Wood">Wood</option>
+            </select>
+        </div>
+        <div class="ys-v-row-item">
+            <label>Condition</label>
+            <select name="condition">
+                <option value="" selected disabled>Any</option>
+                <option value="New">New</option>
+                <option value="Used">Used</option>
+            </select>
+        </div>
+        <div class="ys-v-row-item submit-container">
+            <label>Submit</label>
+            <input type="submit" name="submit" value="Submit"/>
+        </div>
+    </form>
+</div>
