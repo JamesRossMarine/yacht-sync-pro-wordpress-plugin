b49ebe2cd28591f67803182ce8cab7927f2f406e
Brandon Fernandez Creed <brandon.f.creed@gmail.com>
2023-09-20 09:37:46 -0400

    feature/consolidating_css vertical search form now has pagination

--- partials/v-search-form.php
+++ partials/v-search-form.php
@@ -1,6 +1,7 @@
 <div id="v-search-container">
     <form id="ysp-yacht-search-form" class="ys-v-row ysp-yacht-search-form ysp-form" >
-        <div class="ys-v-row-item">
+    <input type="hidden" name="page_index" />    
+    <div class="ys-v-row-item">
             <label>Keyword</label>
             <input type="text" name="ys_keyword" placeholder="Search by Name" />
             <svg class="search-icon" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
