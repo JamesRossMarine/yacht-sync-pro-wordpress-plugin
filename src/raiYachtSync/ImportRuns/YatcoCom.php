30e53af21fd33d0f5b36f2179f6d5e685a6354ad
Josh Hoffman <mail@joshuahoffman.me>
2023-12-28 15:40:28 -0500

    updates

--- src/raiYachtSync/ImportRuns/YatcoCom.php
+++ src/raiYachtSync/ImportRuns/YatcoCom.php
@@ -4,7 +4,7 @@
 		public $yachtBrokerAPIKey = '';
    		public $yachtClientId = '';
    		protected $url = '';
-   		protected $yachtBrokerLimit = 23;
+   		protected $yachtBrokerLimit = 43;
 
 		public function __construct() {
 
@@ -25,8 +25,7 @@
 
 	        $headers = [
 	            'headers' => [
-	                'X-API-KEY'   => $this->yachtBrokerAPIKey,
-	                'X-CLIENT-ID' => $this->yachtClientId
+	             
 	            ]
 	        ];
 
@@ -45,6 +44,7 @@
 				return ['error' => 'Error with auth'];
 			}
 			else {
+				var_dump('BALLS 2948');
 				return ['error' => 'Error http error '.$api_status_code];
 			}
 
@@ -53,15 +53,31 @@
 	        $page = 1;
 
 	        while ($total > $yachtSynced) {
+
+
 	        	$apiUrl  = 'https://www.yatco.com/wp-json/yatco/yachts?page_size='.$this->yachtBrokerLimit;
 
 	        	$apiUrl .='&page_index='.$page;
+
+	        	var_dump($apiUrl);
+
+	        	$page++;
+
+	        	sleep(15);
 
 		        $apiCallWhile = wp_remote_get($apiUrl, $headers);
 		        $apiBody = json_decode($apiCallWhile['body'], true);
 
 		        if (count($apiBody['Results']) == 0) {
+		        	var_dump('returned zero');
+
 		        	break;
+		        }
+
+		        if (! isset($apiBody['Results'])) {
+		        	var_dump($apiBody);
+		        	var_dump(wp_remote_retrieve_response_code($apiCallWhile));
+
 		        }
 
 				foreach ($apiBody['Results'] as $row) {
@@ -72,44 +88,60 @@
 		           	];
 
 		           	$MapToBoatOrg=[
-		           		'YTCID' => 'VesselID',
+		           		'YTC_VESSEL_ID' => 'VesselID',
 		  				'SalesStatus' => 'VesselStatus',
 		                'SaleClassCode' => 'VesselCondition',
 		                'CompanyName' => 'CompanyID' ,
-		                /*'GeneralBoatDescription' => 'Summary' ,
-		                'NumberOfEngines' => 'EngineQty',
-		                'Price' => 'PriceUSD' ,
-		                'NormPrice' => 'PriceUSD',
+
+		                'GeneralBoatDescription' => 'BrokerTeaser' ,
+		                
+		                'Price' => 'AskingPrice' ,
+
+		                //'NormPrice' => 'PriceUSD',
+		                
 		                'ModelYear' => 'Year',
 		                'Model' => 'Model',
-		                'PriceHideInd' => 'PriceHidden',
-		                'MakeString' => 'Manufacturer',
-		                'BoatCategoryCode' => 'Type',
+		                
+		                //'PriceHideInd' => 'PriceHidden',
+		                
+		                'MakeString' => 'BuilderName',
+		                
+		                'BoatCategoryCode' => 'MainCategoryText',
 		                'BoatName' => 'VesselName',
-		                'CruisingSpeedMeasure' => 'CruiseSpeed', 
-		                'MaximumSpeedMeasure' => 'MaximumSpeed',
-		                'RangeMeasure' => 'RangeNMI',
+
+		                //'CruisingSpeedMeasure' => 'CruiseSpeed', 
+		                //'MaximumSpeedMeasure' => 'MaximumSpeed',
+
+		                //'RangeMeasure' => 'RangeNMI',
 		                'BeamMeasure' => 'BeamFeet',
-		                'LastModificationDate' => 'UpdatedTimestamp',
-		                'WaterTankCapacityMeasure' => 'FreshWaterCapacityGallons',
-		                'FuelTankCapacityMeasure' => 'FuelTankCapacityGallons' ,
-		                'DryWeightMeasure' => 'DryWeight' ,
-		                'CabinsCountNumeric' => 'CabinCount',
-		                'HeadsCountNumeric' => 'HeadCount',
+		                
+		                'LastModificationDate' => 'ModifiedDate',
+
+		                //'WaterTankCapacityMeasure' => 'FreshWaterCapacityGallons',
+		                //'FuelTankCapacityMeasure' => 'FuelTankCapacityGallons' ,
+		                
+		                //'DryWeightMeasure' => 'DryWeight' ,
+		                
+		                'CabinsCountNumeric' => 'StateRooms',
+		                
+		                //'HeadsCountNumeric' => 'HeadCount',
+		                
 		                'BoatHullMaterialCode' => 'HullMaterial',
-		                'BoatHullID' => 'HullIdentificationNumber',
-		                'DisplayLengthFeet' => 'LOAFeet',
-		                'TaxStatusCode' => 'TaxStatus',
+		                //'BoatHullID' => 'HullIdentificationNumber',
+		                
+		                //'DisplayLengthFeet' => 'LOAFeet',
+		                //'TaxStatusCode' => 'TaxStatus',
 		                
 		                'NominalLength' => 'LOAFeet',
+
 		                'YSP_LOAFeet' => 'LOAFeet',
-		                'YSP_LOAMeter' => 'LOAMeter',
+		                'YSP_LOAMeter' => 'LOAMeters',
 						
 						'YSP_BeamFeet' => 'BeamFeet',
-		                'YSP_BeamMeter' => 'BeamMeter',
+		                'YSP_BeamMeter' => 'BeamMeters',
 
-		                'AdditionalDetailDescription' => 'Description',
-		                'CabinCountNumeric' => 'CabinCount'*/
+		                //'AdditionalDetailDescription' => 'Description',
+		                //'CabinCountNumeric' => 'CabinCount'
 		           	];
 
 		           	foreach ($MapToBoatOrg as $mapToKey => $key) {
@@ -122,22 +154,50 @@
 
 		           	}
 
-		            if (! empty($row['VesselID'])) {
-		                $find_post=get_posts([
-		                    'post_type' => 'syncing_rai_yacht',
-		                    'meta_query' => [
-		                        array(
-		                           'key' => 'YTCID',
-		                           'value' => $row['VesselID'],
-		                           'compare' => '=',
-		                       )
-		                    ],
-		                ]);
-		            }
-		            else {
-		                $find_post=[];
-		            }
+		           	$detailsUrl = 'https://yatco.com/wp-json/yatco/yacht-detail?vessel_id=' . $row['MLSID'];
+					
+					$apiCallDetails = wp_remote_get($detailsUrl, ['timeout' => 120]);
 
+					$response = $apiCallDetails['body'];
+
+						$response=json_decode($response, true);
+
+					$data = $response;
+
+					if (isset($data['PhotoGallery']) && is_array($data['PhotoGallery'])) {
+	 
+						$reducedImages = array_slice($data['PhotoGallery'], 0, 100);
+
+	                    $reducedImages = array_map(
+	                    	function($img) {
+	                    		$reimg=[
+	                    			'Uri' => $img['largeImageURL']
+	                    		];
+
+	                    		if (! empty($img['Caption'])) {
+	                    			$reimg['Caption']=$img['Caption'];
+	                    		}
+
+	                    		return (object) $reimg;
+	                    	}, 
+	                    	$reducedImages
+	                    );
+
+	                    $theBoat['Images'] = $reducedImages;
+	                    $theBoat['CompanyName'] = $data['Company']['CompanyName'];
+
+					}
+
+	                $find_post=get_posts([
+	                    'post_type' => 'syncing_rai_yacht',
+	                    'meta_query' => [
+	                        array(
+	                           'key' => 'YTC_VESSEL_ID',
+	                           'value' => $row['VesselID']
+	                       )
+	                    ],
+	                ]);
+	           
 		            $post_id=0;
 
 		            if (isset($find_post[0]->ID)) {
@@ -146,7 +206,7 @@
 		                $wpdb->delete($wpdb->postmeta, ['post_id' => $find_post[0]->ID], ['%d']);
 		            }
 
-		            $theBoat['CompanyBoat'] = 1;
+		            var_dump($theBoat);
 
 		            $y_post_id=wp_insert_post(
 		            	apply_filters('raiys_yacht_post', 
