e746f813e0b53770ed171fbde2c9c039e1efbde0
Joshua <mail@joshuahoffman.me>
2024-02-06 17:22:47 -0500

    aws

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -65,16 +65,11 @@ class ComposerAutoloaderInit69c7a48b3b1d1ece5c4baa7772ecf27e
     }
 }
 
-/**
- * @param string $fileIdentifier
- * @param string $file
- * @return void
- */
 function composerRequire69c7a48b3b1d1ece5c4baa7772ecf27e($fileIdentifier, $file)
 {
     if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
         require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
