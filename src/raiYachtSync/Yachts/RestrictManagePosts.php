b8bce12b6d8b89d95a21f3940f6c61949b94541e
Brandon Fernandez Creed <brandon.f.creed@gmail.com>
2024-05-08 17:38:39 -0400

    feature/yachts_search_fields search fields added and working proplery feels good to add this

--- src/raiYachtSync/Yachts/RestrictManagePosts.php
+++ src/raiYachtSync/Yachts/RestrictManagePosts.php
@@ -7,34 +7,39 @@
         }
         
         public function add_actions_and_filters() {
-            add_action( 'restrict_manage_posts', array($this, 'rai_yacht_filtering_by_boat_class'), 10, 1 );
+            // add_action( 'restrict_manage_posts', array($this, 'rai_yacht_filtering_by_boat_class'), 10, 1 );
             add_action( 'restrict_manage_posts', array($this, 'rai_yacht_filtering_by_builder'), 10, 1 );
+            add_action( 'restrict_manage_posts', array($this, 'rai_yacht_filtering_by_condition'), 10, 1 );
+            add_action( 'restrict_manage_posts', array($this, 'rai_yacht_filtering_by_min_length'), 10, 1 );
+            add_action( 'restrict_manage_posts', array($this, 'rai_yacht_filtering_by_max_length'), 10, 1 );
+            add_action( 'restrict_manage_posts', array($this, 'rai_yacht_filtering_by_min_price'), 10, 1 );
+            add_action( 'restrict_manage_posts', array($this, 'rai_yacht_filtering_by_max_price'), 10, 1 );
         }
 
-        public function rai_yacht_filtering_by_boat_class() {
-            if (!is_singular('rai_yacht')) {
-                return;
-            }
+        // public function rai_yacht_filtering_by_boat_class() {
+        //     if (!is_singular('rai_yacht')) {
+        //         return;
+        //     }
 
-            $taxonomies_slugs = array (
-                'boatclass'
-            );
+        //     $taxonomies_slugs = array (
+        //         'boatclass'
+        //     );
 
-            foreach( $taxonomies_slugs as $slug ) {
-                $taxonomy = get_taxonomy($slug);
-                $selected = '';
-                $selected = isset( $_REQUEST[ $slug ] ) ? $_REQUEST[ $slug ] : '';
-                wp_dropdown_categories ( array (
-                    'show_option_all' => __("Show All {$taxonomy->label}"),
-                    'taxonomy' => $slug,
-                    'name' => $slug,
-                    'orderby' => 'name',
-                    'selected' => $selected,
-                    'show_count' => true,
-                    'hide_empty' => true,
-                ) );
-            }
-        }
+        //     foreach( $taxonomies_slugs as $slug ) {
+        //         $taxonomy = get_taxonomy($slug);
+        //         $selected = '';
+        //         $selected = isset( $_REQUEST[ $slug ] ) ? $_REQUEST[ $slug ] : '';
+        //         wp_dropdown_categories ( array (
+        //             'show_option_all' => __("Show All {$taxonomy->label}"),
+        //             'taxonomy' => $slug,
+        //             'name' => $slug,
+        //             'orderby' => 'name',
+        //             'selected' => $selected,
+        //             'show_count' => true,
+        //             'hide_empty' => true,
+        //         ) );
+        //     }
+        // }
 
         public function rai_yacht_filtering_by_builder($post_type) {
             if($post_type != 'rai_yacht'){
@@ -63,5 +68,114 @@
             }
 
             echo '</select>';
+        }
+
+        public function rai_yacht_filtering_by_condition($post_type) {
+            if($post_type != 'rai_yacht'){
+                return;
+            }
+            
+            global $wpdb;
+
+            $selected = '';
+            $request_attr = 'condition';
+            $meta_key = 'SaleClassCode';
+
+            if ( isset($_REQUEST[$request_attr]) ) {
+            
+                $selected = $_REQUEST[$request_attr];
+            }
+
+            $results = $this->DBHelper->get_unique_yacht_meta_values($meta_key);
+            
+            echo '<select name="condition">';
+            echo '<option value="">' . __( 'Any condition', 'ysp.local' ) . ' </option>';
+
+            foreach($results as $condition){
+                $select = ($condition == $selected) ? ' selected="selected"':'';
+                echo '<option value="' . $condition . '"' . $select . '>' . $condition . ' </option>';
+            }
+
+            echo '</select>';
+        }
+
+        public function rai_yacht_filtering_by_min_length($post_type) {
+            if($post_type != 'rai_yacht'){
+                return;
+            }
+            
+            global $wpdb;
+
+            $selected = '';
+            $request_attr = 'lengthlo';
+            $meta_key = 'YSP_Length';
+
+            if ( isset($_REQUEST[$request_attr]) ) {
+            
+                $selected = $_REQUEST[$request_attr];
+            }
+
+            $results = $this->DBHelper->get_unique_yacht_meta_values($meta_key);
+            echo '<input type="number" name="lengthlo" placeholder="Any minimum length" value="' . esc_attr($selected) . '" />';
+        }
+
+        public function rai_yacht_filtering_by_max_length($post_type) {
+            if($post_type != 'rai_yacht'){
+                return;
+            }
+            
+            global $wpdb;
+
+            $selected = '';
+            $request_attr = 'lengthhi';
+            $meta_key = 'YSP_Length';
+
+            if ( isset($_REQUEST[$request_attr]) ) {
+            
+                $selected = $_REQUEST[$request_attr];
+            }
+
+            $results = $this->DBHelper->get_unique_yacht_meta_values($meta_key);
+            echo '<input type="number" name="lengthhi" placeholder="Any maximum length" value="' . esc_attr($selected) . '" />';
+        }
+
+        public function rai_yacht_filtering_by_min_price($post_type) {
+            if($post_type != 'rai_yacht'){
+                return;
+            }
+            
+            global $wpdb;
+
+            $selected = '';
+            $request_attr = 'pricelo';
+            $meta_key = 'NormPrice';
+
+            if ( isset($_REQUEST[$request_attr]) ) {
+            
+                $selected = $_REQUEST[$request_attr];
+            }
+
+            $results = $this->DBHelper->get_unique_yacht_meta_values($meta_key);
+            echo '<input type="number" name="pricelo" placeholder="Any minimum price" value="' . esc_attr($selected) . '" />';
+        }
+
+        public function rai_yacht_filtering_by_max_price($post_type) {
+            if($post_type != 'rai_yacht'){
+                return;
+            }
+            
+            global $wpdb;
+
+            $selected = '';
+            $request_attr = 'pricehi';
+            $meta_key = 'NormPrice';
+
+            if ( isset($_REQUEST[$request_attr]) ) {
+            
+                $selected = $_REQUEST[$request_attr];
+            }
+
+            $results = $this->DBHelper->get_unique_yacht_meta_values($meta_key);
+            echo '<input type="number" name="pricehi" placeholder="Any maximum price" value="' . esc_attr($selected) . '" />';
         }
     }
