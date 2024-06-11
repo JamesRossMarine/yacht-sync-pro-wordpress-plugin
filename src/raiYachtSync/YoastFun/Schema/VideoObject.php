3e45af19a4b0ef98ba47ec330c8c3231d957d666
JOSH <wizardintech@gmail.com>
2024-06-11 10:28:22 -0400

    seo - video object update

--- src/raiYachtSync/YoastFun/Schema/VideoObject.php
+++ src/raiYachtSync/YoastFun/Schema/VideoObject.php
@@ -7,8 +7,9 @@
         $this->context = $context;
         $this->options = new raiYachtSync_Options();
 
-        if (isset($this->context->post)) {
+        if (is_singular('rai_yacht') && isset($this->context->post)) {
             $this->context->video = get_post_meta($this->context->post->ID, 'Videos', true);
+            $this->context->imagess = get_post_meta($this->context->post->ID, 'Images', true);
         }
 
         $this->youtube_data_api_key = $this->options->get('youtube_data_api_key');
@@ -24,40 +25,55 @@
 
         $youtube_data_api_key = $this->youtube_data_api_key;
 
-        $url = $this->context->video->url[0];
-        $url_without_query = strstr($url, "?", true);
-        $url_parts = explode("/", $url_without_query);
-        $video_id = end($url_parts);
+        if ($youtube_data_api_key != "") {
 
+            $url = $this->context->video->url[0];
 
-        $api_url = "https://www.googleapis.com/youtube/v3/videos?id=" . $video_id . "&key=" . $youtube_data_api_key . "&part=contentDetails,snippet";
+            preg_match('%(?:youtube(?:-nocookie)?\.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)/|.*[?&]v=)|youtu\.be/)([^"&?/ ]{11})%i', $url, $match);    
+            
+            $video_id = $match[1];
 
-        $youtube_data_response = wp_remote_get(
-            esc_url_raw( $api_url ),
-            array(
-                'headers' => array(
-                    'referer' => home_url()
-                )
-            )
-        );
+            if ($video_id != '') {
 
-        $youtube_data_response = json_decode(wp_remote_retrieve_body($youtube_data_response));
-        $youtube_description = $youtube_data_response->items[0]->snippet->description;
-        $youtube_description = strip_tags($youtube_description);
-        $youtube_description = preg_replace('/[^a-zA-Z0-9\s]/', '', $youtube_description);
+                $api_url = "https://www.googleapis.com/youtube/v3/videos?id=" . $video_id . "&key=" . $youtube_data_api_key . "&part=contentDetails,snippet";
 
-        if (isset($youtube_data_api_key) && $youtube_data_api_key != "") {
-            $data = [
-                "@type" => "VideoObject",
-                "name" => $this->context->post->post_title . " Video",
-                "description" => $youtube_description,
-                "thumbnailUrl" => $this->context->video->thumbnailUrl[0],
-                "contentUrl" => $this->context->video->url[0],
-                "uploadDate" => $youtube_data_response->items[0]->snippet->publishedAt,
-                "duration" => $youtube_data_response->items[0]->contentDetails->duration
-            ];
+                $youtube_data_response = wp_remote_get(
+                    esc_url_raw( $api_url ),
+                    array(
+                        'headers' => array(
+                            'referer' => home_url()
+                        )
+                    )
+                );
+
+                $youtube_data_response = json_decode(wp_remote_retrieve_body($youtube_data_response));
+                $youtube_description = $youtube_data_response->items[0]->snippet->description;
+                $youtube_description = strip_tags($youtube_description);
+                $youtube_description = preg_replace('/[^a-zA-Z0-9\s]/', '', $youtube_description);
+
+                $thumbnailUrl = $this->context->video->thumbnailUrl[0];
+
+                if (empty($thumbnailUrl)) {
+                    $thumbnailUrl = $this->context->imagess[0]->Uri;
+                }
+
+                if (empty($youtube_description)) {
+                    $youtube_description = $this->context->post->post_title;
+                }
+
+                $data = [
+                  "@type" => "VideoObject",
+                    "name" => $this->context->post->post_title . " Video",
+                    "description" => $youtube_description,
+                    "thumbnailUrl" => $thumbnailUrl,
+                    "contentUrl" => $this->context->video->url[0],
+                    "uploadDate" => $youtube_data_response->items[0]->snippet->publishedAt,
+                    "duration" => $youtube_data_response->items[0]->contentDetails->duration
+                ];
+
+                return $data;   
+            }
     
-            return $data;
         }
 
         return [];
